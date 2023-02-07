1a60558a4633e7bd7622f610728daa2658045c11
musto74 <124668953+musto74@users.noreply.github.com>
2023-02-07 11:49:05 +0100

    Add files via upload

--- musto.c
+++ musto.c
@@ -3,14 +3,20 @@
 int main()
 {
 
-    char x = 'a';
-    char y = 'b';
+    int a = 7;
+    int b = 3;
+    int divisione = 7 / 3;
+    int resto = 7 % 3;
+    int somma = 7 + 3;
+    int sott = 7 - 3;
+    int molt = 7 * 3;
+
     
-    printf("x era: %c\n", x);
-    printf("y era: %c\n", y);
-    printf("inserisci due caratteri \n");
-    scanf(" %c %c ", &x, &y);
-    printf("x e diventato: %c\n", x);
-    printf("y e diventato: %c\n", y);
+    printf("la divisione di a e b e: %d\n", divisione);
+    printf("il resto di a e b e: %d\n", resto);
+    printf("il risultato di a e b e; %d\n", somma);
+    printf("il risultato di a e b e; %d\n", sott);
+    printf("il prodotto di a e b e; %d\n", molt);
+   
 
 }
---
+++ musto2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{ 
+    int a ;
+    int b ;
+    int risultato;
+
+   
+   
+    printf("inserisci i valori di a e b\n");
+    scanf("%d %d", &a , &b);
+    risultato = a / b ;
+    printf("il risultato e: %d\n", risultato);
+    
+    
+    }
