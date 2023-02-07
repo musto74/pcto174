9a9256d677b95ef62f48a1553c27b1c2485e8cbb
musto74 <124668953+musto74@users.noreply.github.com>
2023-02-07 16:44:29 +0100

    Add files via upload

---
+++ efile.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    
+
+    printf("inserire tre numeri interi\n");
+    scanf("%d %d %d", &a, &b, &c);
+    if (b-a==c-b)
+    {
+        printf("sono in progressione aritmetica\n");
+
+    }
+   else
+   {
+    printf("non sono in progressione aritmetica\n");
+   }
+    
+
+
+
+
+
+}
