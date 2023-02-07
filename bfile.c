ed34a194d66382a7ec4e4c2f901810e6369f2b43
musto74 <124668953+musto74@users.noreply.github.com>
2023-02-07 15:05:07 +0100

    Add files via upload

---
+++ bfile.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int eta;
+
+    printf("qual e la tua eta\n");
+    scanf("%d", &eta);
+    if (eta >= 18)
+    {printf("l'utente e maggiorenne\n");}
+    else 
+    {printf("l'utente e minorenne\n");}
+
+
+
+
+
+}
