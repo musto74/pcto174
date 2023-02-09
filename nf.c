83bd4084f48c8a010cd07b3e1e3806f5a044b848
musto74 <124668953+musto74@users.noreply.github.com>
2023-02-09 09:24:15 +0100

    Add files via upload

---
+++ nf.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=0;
+    int y=0;
+    float somma=0;
+    while (x>=0)
+    {
+    printf("inserisci un numero\n");
+    scanf("&d", &x);
+    y=y+1;
+    somma=somma+x;
+    printf("%f", somma/y);
+
+    }
+
+
+
+}
